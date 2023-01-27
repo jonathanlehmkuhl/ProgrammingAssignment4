4af1ceb5531e17e9f76caed0c023bc3532604401
Jonathan Lehmkuhl <jonathanlehmkuhl@gmail.com>
2023-01-27 10:03:47 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,14 +10,14 @@ library(dplyr)
 # Extracting measurements, activities and subjects of the training data set.
 # All columns are combined into the data frame df_training.
 measurements_training <- read.table("UCI HAR Dataset/train/X_train.txt")
-activities_training <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "Activity")
-subject_training <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
+activities_training <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+subject_training <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
 df_training <- cbind(measurements_training, activities_training, subject_training)
 
 # Same procedure for the test data set
 measurements_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-activities_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "Activity")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
+activities_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 df_test <- cbind(measurements_test, activities_test, subject_test)
 
 # Combining the rows of both data sets df_training and df_test
@@ -30,18 +30,21 @@ names(df_merged)[1:561] <- variable_names[, 2]
 
 # Creating the first tidy data set df:
 # Selecting only the columns that measure mean and std for a particular measurement
-df <- select(df_merged, grep("mean\\(\\)|std\\(\\)", names(df_merged)), "Activity", "Subject")
+df <- select(df_merged, grep("mean\\(\\)|std\\(\\)", names(df_merged)), "activity", "subject")
 
 # Extracting the activity labels and using them to get descriptive values
-# for the factor variable Activity.
-# Making the variable Subject of class factor.
+# for the factor variable activity.
+# Making the variable subject of class factor.
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
-df$Activity <- factor(df$Activity, labels = activity_labels[, 2])
-df$Subject <- as.factor(df$Subject)
+df$activity <- factor(df$activity, labels = activity_labels[, 2])
+df$subject <- as.factor(df$subject)
 
 # Creating the second tidy data set df_summarized:
-# Grouping df by Activity and Subject and summarizing the other columns
+# Grouping df by activity and subject and summarizing the other columns
 # by applying the mean function
-df_summarized <- df %>% group_by(Activity, Subject) %>% summarize_all(mean)
-# Adding the prefix "average" for all variable names besides Activity and Subject
+df_summarized <- df %>% group_by(activity, subject) %>% summarize_all(mean)
+# Adding the prefix "average" for all variable names besides activity and subject
 names(df_summarized)[-(1:2)] <- paste("average", names(df_summarized)[-(1:2)])
+
+# Output
+df_summarized
